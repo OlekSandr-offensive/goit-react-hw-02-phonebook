fc8dd854bd04a8bcf6e01774be79c74244ff443c
Oleksandr Tarasiuk <oleksandr.tarasyuk92@gmail.com>
2021-07-24 21:33:07 +0200

    finish work

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import shortid from 'shortid';
+import './App.css';
+import ContactList from './components/contacts/ContactList';
+import initialContacts from './contacts.json';
+import ContactForm from './components/contactForm/ContactForm';
+import Filter from './components/filter/Filter';
+
+class App extends React.Component {
+  state = {
+    contacts: initialContacts,
+    filter: '',
+    name: '',
+    number: '',
+  };
+
+  FormSubmitHandler = ({ name, number }) => {
+    const { contacts } = this.state;
+    const findContact = contacts.find(
+      contact => contact.name.toLowerCase() === name.toLowerCase(),
+    );
+    if (findContact) {
+      alert(`${name} is already in contacts.`);
+    } else {
+      const AddContact = {
+        id: shortid.generate(),
+        name: name,
+        number: number,
+      };
+      this.setState(({ contacts }) => ({
+        contacts: [AddContact, ...contacts],
+      }));
+    }
+  };
+
+  deleteContacts = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  changeFilter = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
+  };
+
+  render() {
+    const { filter } = this.state;
+    const visibleContacts = this.getVisibleContacts();
+
+    return (
+      <div className="App">
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.FormSubmitHandler} />
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactList
+          contacts={visibleContacts}
+          onDeleteContacts={this.deleteContacts}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
